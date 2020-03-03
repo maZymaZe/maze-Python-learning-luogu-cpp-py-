c3528a5446fdac364dea3c2bf11c74383b9e2831
maze <782618517@qq.com>
2020-03-03 19:02:06 +0800

    后+中->先序遍历

---
+++ luogu/cpp/p1030.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+char h[10],z[10];
+int l;
+int fx(int x){
+    for(int i=0;i<l;i++){
+        if(z[i]==h[x])return i;
+    }
+}
+void dfs(int l1,int r1,int l2,int r2){
+    
+    if(l1>r1||l2>r2)return;
+    if(l1==r1){
+        printf("%c",z[l1]);
+        return;
+    }
+    int y=fx(r2);
+    printf("%c",h[r2]);
+    if(r1-1>=l1)
+    dfs(l1,y-1,l2,l2+(y-1-l1));
+    if(r1>=y+1)
+    dfs(y+1,r1,l2+(y-l1),r2-1);
+
+}
+int main(){
+    scanf("%s%s",z,h);
+    l=strlen(z);
+    dfs(0,l-1,0,l-1);
+    return 0;
+}
