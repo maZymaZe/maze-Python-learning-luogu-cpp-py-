90021fb2787ccca646840705e8e2077de9cd097c
maze <782618517@qq.com>
2020-03-23 19:44:55 +0800

    ????

---
+++ LeetCode/m40.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def getLeastNumbers(self, arr, k):
+        """
+        :type arr: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        arr.sort()
+        return arr[0:k]
