5472adea4cda80b60a86f143634839a49e4f5be2
maze <782618517@qq.com>
2020-03-31 15:49:21 +0800

    stl

---
+++ LeetCode/l46.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> a;
+        a.push_back(nums);
+        long long l=1;
+        int ll=nums.size();
+        for(int i=1;i<=nums.size();i++)l*=i;
+        for(int i=1;i<l;i++){
+            next_permutation(nums.begin(),nums.end());
+            a.push_back(nums);
+        }
+        return a;
+    }
+};
