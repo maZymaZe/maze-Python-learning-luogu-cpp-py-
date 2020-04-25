e7a6a36d036f0ce09c72c6e477455890f8300e71
maze <782618517@qq.com>
2020-04-25 08:51:01 +0800

    划水

---
+++ LeetCode/l13.py
@@ -0,0 +1,13 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        t={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        ans=0
+        i=0
+        while i<len(s):
+            if i!= len(s)-1 and t[s[i]]<t[s[i+1]]:
+                ans+=t[s[i+1]]-t[s[i]]
+                i+=1
+            else:
+                ans+=t[s[i]]
+            i+=1
+        return ans
