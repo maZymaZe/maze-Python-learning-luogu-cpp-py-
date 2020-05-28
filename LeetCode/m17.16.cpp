06f7297e7e3f83e29f100a9f3098a4f843b0d1b4
maze <782618517@qq.com>
2020-05-29 07:51:58 +0800

    迷惑重题

---
+++ LeetCode/m17.16.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int massage(vector<int>& nums) {
+         int l=nums.size();
+        if(l==0)return 0;
+        if(l==1)return  nums[0];
+        if(l==2)return max(nums[1],nums[0]);
+        if(l==3)return max(nums[0]+nums[2],nums[1]);
+        vector<int> dp;
+        dp.push_back(nums[0]);
+        dp.push_back(max(nums[1],nums[0]));
+        dp.push_back(max(nums[0]+nums[2],nums[1]));
+        for(int i=3;i<l;i++){
+            dp.push_back(nums[i]+max(dp[i-2],dp[i-3]));
+        }
+        return max(dp[l-1],dp[l-2]);
+    }
+};
