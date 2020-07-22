f17798510e7b2c2d99173c1e34efbcc3fa5fd28c
maze <782618517@qq.com>
2020-07-22 08:17:20 +0800

    二分

---
+++ LeetCode/j11.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minArray(vector<int>& numbers) {
+        int l=0,r=numbers.size()-1;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(numbers[mid]>numbers[r])l=mid+1;
+            else if(numbers[mid]<numbers[r])r=mid;
+            else r--;
+        }
+        return numbers[l];
+    }
+};
