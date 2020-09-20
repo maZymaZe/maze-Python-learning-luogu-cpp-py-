41e1cb50d25dc731587bd49b7c6cea75c56916f6
maze <782618517@qq.com>
2020-09-21 07:11:16 +0800

    遍历

---
+++ LeetCode/l538.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int tot=0;
+    void dfs(TreeNode* root){
+        if(!root)return;
+        dfs(root->right);
+        tot+=root->val;
+        root->val=tot;
+        dfs(root->left);
+    }
+    TreeNode* convertBST(TreeNode* root) {
+        dfs(root);
+        return root;
+    }
+};
