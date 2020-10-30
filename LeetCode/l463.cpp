39bee3f1ea76afc0ff4991e9ecc00c97f06c5d98
maze <782618517@qq.com>
2020-10-30 11:49:33 +0800

    划水

---
+++ LeetCode/l463.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size(),ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]){
+                    if(j==0||grid[i][j-1]==0)ans++;
+                    if(i==0||grid[i-1][j]==0)ans++;
+                    if(j==m-1||grid[i][j+1]==0)ans++;
+                    if(i==n-1||grid[i+1][j]==0)ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
