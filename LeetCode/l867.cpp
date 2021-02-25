6a2d79964d863c85cd57ad19a7c7384f7aff3865
maze <782618517@qq.com>
2021-02-25 08:33:50 +0800

    划水

---
+++ LeetCode/l867.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int n=matrix.size(),m=matrix[0].size();
+        vector<vector<int>> ans(m, vector<int>(n));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans[i][j]=matrix[j][i];
+            }
+        }
+        return ans;
+    }
+};
