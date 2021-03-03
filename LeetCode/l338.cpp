a821408ff96163790fe89eec44a485dd6670d76c
maze <782618517@qq.com>
2021-03-03 10:24:21 +0800

    dp

---
+++ LeetCode/l338.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> ans(num+1);
+        ans[0]=0;
+        for(int i=1;i<=num;i++)ans[i]=ans[i>>1]+(i&1);
+        return ans;
+    }
+};
