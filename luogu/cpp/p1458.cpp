74de5c01808253b3b6239805401b3fdd51778296
maze <782618517@qq.com>
2021-03-28 00:46:29 +0800

    数学

---
+++ luogu/cpp/p1458.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+int n;
+void dfs(int a,int b, int c,int d){
+    if(b>n||d>n)return;
+    dfs(a,b,a+c,b+d);
+    if(b+d<=n){
+        printf("%d/%d\n",a+c,b+d);
+    }
+    dfs(a+c,b+d,c,d);
+}
+int main(){
+    scanf("%d",&n);
+    printf("0/1\n");
+    dfs(0,1,1,1);
+    printf("1/1\n");
+    return 0;
+}
