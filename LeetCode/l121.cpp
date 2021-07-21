cb30254b3ee3d3c05c6d3f0fa0fc722f4ef43a28
maze <782618517@qq.com>
2021-07-21 11:22:43 +0800

    划水

---
+++ LeetCode/l121.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mi=0x3f3f3f3f,l=prices.size(),ans=0;
+        for(int i=0;i<l;i++){
+            if(prices[i]-mi>ans)ans=prices[i]-mi;
+            if(mi>prices[i])mi=prices[i];
+        }
+        return ans;
+
+    }
+};
