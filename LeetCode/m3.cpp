94565a8d0c36118d8aaffb23bff5528c714a455e
maze <782618517@qq.com>
2021-08-01 00:49:28 +0800

    划水

---
+++ LeetCode/m3.cpp
@@ -0,0 +1,12 @@
+class Solution {
+   public:
+    int findRepeatNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int l = nums.size();
+        for (int i = 0; i < l - 1; i++) {
+            if (nums[i] == nums[i + 1])
+                return nums[i];
+        }
+        return -1;
+    }
+};
