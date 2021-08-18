8a65a90b34801712f3cffd693c163c2d908ad3a1
maze <782618517@qq.com>
2021-08-19 00:49:48 +0800

    ****

---
+++ LeetCode/l434.cpp
@@ -0,0 +1,13 @@
+class Solution {
+   public:
+    int countSegments(string s) {
+        s += ' ';
+        int cnt = 0;
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (s[i] != ' ' && s[i + 1] == ' ') {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
