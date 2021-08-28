370e1ee2f3da054d432d7a2f423398a95373e8bc
maze <782618517@qq.com>
2021-08-28 10:26:27 +0800

    dfs

--- LeetCode/l282.cpp
+++ LeetCode/l282.cpp
@@ -1,152 +1,53 @@
 class Solution {
    public:
-    string exp;
-    vector<string> ans;
-    int l, el, tg, op[4] = {'+', '-', '*', '$'};
-    stack<long long> st;
-    stack<int> oo;
-    long long cal() {
-        for (int i = 0; i < el; i++) {
-            if (exp[i] >= '0' && exp[i] <= '9')
-                st.push(exp[i] - '0');
-            else if (exp[i] == '+' || exp[i] == '-') {
-                while (!oo.empty()) {
-                    int tt = oo.top();
-                    oo.pop();
-                    long long a, b;
-                    b = st.top(), st.pop(), a = st.top(), st.pop();
-                    if (tt == '+')
-                        st.push(a + b);
-                    if (tt == '-')
-                        st.push(a - b);
-                    if (tt == '*')
-                        st.push(a * b);
-                    if (tt == '$') {
-                        int ll = 0;
-                        long long s = b;
-                        while (s)
-                            s /= 10, ll++;
-                        if (!ll)
-                            ll++;
-                        while (ll) {
-                            a *= 10;
-                            ll--;
-                        }
-                        st.push(a + b);
-                    }
-                }
-                oo.push(exp[i]);
-            } else if (exp[i] == '*') {
-                while (!oo.empty() && (oo.top() == '*' || oo.top() == '$')) {
-                    int tt = oo.top();
-                    oo.pop();
-                    long long a, b;
-                    b = st.top(), st.pop(), a = st.top(), st.pop();
-
-                    if (tt == '*')
-                        st.push(a * b);
-                    if (tt == '$') {
-                        int ll = 0;
-                        long long s = b;
-                        while (s)
-                            s /= 10, ll++;
-                        if (!ll)
-                            ll++;
-                        while (ll) {
-                            a *= 10;
-                            ll--;
-                        }
-                        st.push(a + b);
-                    }
-                }
-                oo.push(exp[i]);
-            } else if (exp[i] == '$') {
-                while (!oo.empty() && (oo.top() == '$')) {
-                    int tt = oo.top();
-                    oo.pop();
-                    long long a, b;
-                    b = st.top(), st.pop(), a = st.top(), st.pop();
-
-                    if (tt == '$') {
-                        int ll = 0;
-                        long long s = b;
-                        while (s)
-                            s /= 10, ll++;
-                        if (!ll)
-                            ll++;
-                        while (ll) {
-                            a *= 10;
-                            ll--;
-                        }
-                        st.push(a + b);
-                    }
-                }
-                oo.push(exp[i]);
-            }
-        }
-        while (!oo.empty()) {
-            int tt = oo.top();
-            oo.pop();
-            long long a, b;
-            b = st.top(), st.pop(), a = st.top(), st.pop();
-            if (tt == '+')
-                st.push(a + b);
-            if (tt == '-')
-                st.push(a - b);
-            if (tt == '*')
-                st.push(a * b);
-            if (tt == '$') {
-                int ll = 0;
-                long long s = b;
-                while (s)
-                    s /= 10, ll++;
-                if (!ll)
-                    ll++;
-                while (ll) {
-                    a *= 10;
-                    ll--;
-                }
-                st.push(a + b);
-            }
-        }
-        long long ret = st.top();
-        while (!st.empty())
-            st.pop();
-        while (!oo.empty())
-            oo.pop();
-        return ret;
+    vector<string> addOperators(string num, int target) {
+        vector<string> result;
+        string track = "";
+        backtrack(num, target, 0, 0, 1, result, track);
+        return result;
     }
-    void dfs(int pos) {
-        if (pos == el) {
-            for (int i = 0; i < el - 1; i++) {
-                if (exp[i] == '0' && (i == 0 || exp[i - 1] != '$') &&
-                    exp[i + 1] == '$')
-                    return;
-            }
-            if (cal() == tg) {
-                string t;
-                for (int i = 0; i < el; i++) {
-                    if (exp[i] != '$')
-                        t += exp[i];
-                }
-                ans.push_back(t);
-            }
+    //注：mult表示的上一步的操作数
+    void backtrack(string num,
+                   int target,
+                   int index,
+                   long val,
+                   long mult,
+                   vector<string>& result,
+                   string& track) {
+        if (index == num.size()) {
+            if (val == target)
+                result.push_back(track);
             return;
         }
-        for (int i = 0; i < 4; i++) {
-            exp[pos] = op[i];
-            dfs(pos + 2);
+        int len = track.size();
+        for (int i = index; i < num.length(); i++) {
+            //转换数字
+            string sVal = num.substr(index, i - index + 1);
+            long n = stol(sVal);
+            //第一个数字，不需要加符号
+            if (index == 0) {
+                track += sVal;
+                backtrack(num, target, i + 1, n, n, result, track);
+                track.resize(len);
+            } else {
+                // +
+                track += "+" + sVal;
+                backtrack(num, target, i + 1, val + n, n, result, track);
+                track.resize(len);
+                // -
+                track += "-" + sVal;
+                backtrack(num, target, i + 1, val - n, -n, result, track);
+                track.resize(len);
+                // *
+                track += "*" + sVal;
+                //由于乘法的优先级比加、减法高，所以需要回退到上一步，即把上一步的操作数与乘法进行运算
+                //比如2+3*2，我们在3后面添加*，然而*的优先级高，所以val(5)-mult(3)返回上一步，然后2+3*2=8
+                backtrack(num, target, i + 1, val - mult + mult * n, mult * n,
+                          result, track);
+                track.resize(len);
+            }
+            if (n == 0)
+                return;
         }
-    }
-    vector<string> addOperators(string num, int target) {
-        l = num.size();
-        el = num.size() * 2 - 1;
-        tg = target;
-        exp = string(el, '0');
-        for (int i = 0; i < l; i++) {
-            exp[i * 2] = num[i];
-        }
-        dfs(1);
-        return ans;
     }
 };
