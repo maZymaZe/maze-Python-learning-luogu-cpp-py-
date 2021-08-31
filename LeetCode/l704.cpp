f92caa2e8b0f0e20318a3c13ace016527ddd33ae
maze <782618517@qq.com>
2021-09-01 07:23:25 +0800

    划水

---
+++ LeetCode/l704.cpp
@@ -0,0 +1,9 @@
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        auto it = lower_bound(nums.begin(), nums.end(), target);
+        if (it == nums.end() || *it != target)
+            return -1;
+        return it - nums.begin();
+    }
+};
