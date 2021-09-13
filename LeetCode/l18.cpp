2ee4a4d6e7cccd8af6e19dad7f73b9f1a0b0ae2d
maze <782618517@qq.com>
2021-09-14 00:11:53 +0800

    fix

--- LeetCode/l18.cpp
+++ LeetCode/l18.cpp
@@ -9,20 +9,20 @@ class Solution {
         for (int i = 0; i < l - 3; ++i) {
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
-            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target)
+            if (0ll+nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target)
                 break;
-            if (nums[i] + nums[l - 1] + nums[l - 2] + nums[l - 3] < target)
+            if (0ll+nums[i] + nums[l - 1] + nums[l - 2] + nums[l - 3] < target)
                 continue;
             for (int j = i + 1; j < l - 2; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1])
                     continue;
-                if (nums[j] + nums[j + 1] + nums[j + 2] + nums[i] > target)
+                if (0ll+nums[j] + nums[j + 1] + nums[j + 2] + nums[i] > target)
                     break;
-                if (nums[j] + nums[i] + nums[l - 1] + nums[l - 2] < target)
+                if (0ll+nums[j] + nums[i] + nums[l - 1] + nums[l - 2] < target)
                     continue;
                 int left = j + 1, right = l - 1;
                 while (left < right) {
-                    int s = nums[i] + nums[j] + nums[left] + nums[right];
+                    int s = 0ll+nums[i] + nums[j] + nums[left] + nums[right];
                     if (s == target) {
                         ans.push_back(
                             {nums[i], nums[j], nums[left], nums[right]});
