218fe09f2c7b7ad5c9ea5ef8130192758286c0ed
maze <782618517@qq.com>
2021-09-21 00:09:52 +0800

    递归

---
+++ LeetCode/l450.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root)
+            return nullptr;
+        if (key > root->val)
+            root->right = deleteNode(root->right, key);
+        else if (key < root->val)
+            root->left = deleteNode(root->left, key);
+        else {
+            if (!root->right)
+                return root->left;
+            if (!root->left)
+                return root->right;
+            TreeNode* node = root->right;
+            while (node->left)
+                node = node->left;
+            node->left = root->left;
+            root = root->right;
+        }
+        return root;
+    }
+};
