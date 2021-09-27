cf4c72e3d38fc3f3e36ee84fcb08d701f7be9f96
maze <782618517@qq.com>
2021-09-28 00:30:13 +0800

    划水

---
+++ LeetCode/l507.cpp
@@ -0,0 +1,7 @@
+class Solution {
+   public:
+    bool checkPerfectNumber(int num) {
+        return num == 6 || num == 28 || num == 496 || num == 8128 ||
+               num == 33550336;
+    }
+};
