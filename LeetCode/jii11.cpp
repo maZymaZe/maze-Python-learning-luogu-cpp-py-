ce55bc3ac735dcbbe67e18ec9e7a90bf74fcff37
maze <782618517@qq.com>
2021-10-18 00:07:18 +0800

    划水

---
+++ LeetCode/jii11.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    int findMaxLength(vector<int>& nums) {
+        int l = nums.size(), ans = 0;
+        vector<int> s(2 * l + 1), t(2 * l + 1);
+        int b = l;
+
+        for (int i = 0; i < l; i++) {
+            b += (nums[i] == 1) ? 1 : -1;
+            s[b] = i + 1;
+            if (t[b] == 0)
+                t[b] = i + 1;
+            if (s[b] - t[b] + 1 > ans)
+                ans = s[b] - t[b];
+        }
+        ans = max(ans, s[l]);
+        return ans;
+    }
+};
