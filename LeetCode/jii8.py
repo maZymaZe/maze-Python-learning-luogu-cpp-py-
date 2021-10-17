5605939509a9d46351280678d0d642aee0e5b4ff
maze <782618517@qq.com>
2021-10-17 11:56:46 +0800

    划水

---
+++ LeetCode/jii8.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+        l = len(nums)
+        flag = False
+        begin = 0
+        end = 0
+        tot = 0
+        ans = 0
+        while begin < l:
+            while end < l and tot < s:
+                tot += nums[end]
+                end += 1
+            if tot >= s:
+                if flag == False:
+                    ans = end-begin
+                    flag = True
+                else:
+                    ans = min(ans, end-begin)
+            tot -= nums[begin]
+            begin += 1
+        return ans
