2405dc82a2183b5d47443b6886e72382655da0d8
maze <782618517@qq.com>
2021-10-21 00:08:10 +0800

    划水

---
+++ LeetCode/jii22.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+   public:
+    ListNode* detectCycle(ListNode* head) {
+        auto f = head;
+        auto s = head;
+        while (1) {
+            if (!f)
+                return nullptr;
+            f = f->next;
+            s = s->next;
+            if (!f)
+                return nullptr;
+            f = f->next;
+            if (f && f == s)
+                break;
+        }
+        f = head;
+        while (1) {
+            if (f == s)
+                return f;
+            f = f->next;
+            s = s->next;
+                }
+        return nullptr;
+    }
+};
