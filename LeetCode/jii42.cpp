ab399ab64e20e6614a1fbed9cbb446f5aaeb2131
maze <782618517@qq.com>
2021-10-27 00:15:57 +0800

    划水

---
+++ LeetCode/jii42.cpp
@@ -0,0 +1,21 @@
+class RecentCounter {
+   public:
+    queue<int> q;
+    RecentCounter() {
+        while (!q.empty())
+            q.pop();
+    }
+
+    int ping(int t) {
+        q.push(t);
+        while (t - q.front() > 3000)
+            q.pop();
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
