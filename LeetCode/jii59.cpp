7f8a9fee1623a3ba349c6383789910d4b2d8fb51
maze <782618517@qq.com>
2021-11-01 00:04:24 +0800

    划水

---
+++ LeetCode/jii59.cpp
@@ -0,0 +1,26 @@
+class KthLargest {
+   public:
+    priority_queue<int, vector<int>, greater<int>> q;
+    int t;
+    KthLargest(int k, vector<int>& nums) {
+        t = k;
+        for (int i : nums) {
+            q.push(i);
+            if (q.size() > k)
+                q.pop();
+        }
+    }
+
+    int add(int val) {
+        q.push(val);
+        if (q.size() > t)
+            q.pop();
+        return q.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
