fadc23b26c147b0e0e1e80e55393508aa3c136b4
maze <782618517@qq.com>
2021-11-11 00:02:58 +0800

    划水

---
+++ LeetCode/jii85.cpp
@@ -0,0 +1,17 @@
+class Solution {
+   public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int ans = 0, l = -1, r = -2, len = timeSeries.size();
+        sort(timeSeries.begin(), timeSeries.end());
+        for (int i = 0; i < len; i++) {
+            if (timeSeries[i] > r) {
+                ans += r - l + 1;
+                l = timeSeries[i], r = timeSeries[i] + duration - 1;
+            } else {
+                r = timeSeries[i] + duration - 1;
+            }
+        }
+        ans += r - l + 1;
+        return ans;
+    }
+};
