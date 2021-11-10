667892005b8f776e6e38ce9dcfac914b44138f66
maze <782618517@qq.com>
2021-11-11 00:02:54 +0800

    划水

---
+++ LeetCode/jii84.cpp
@@ -0,0 +1,31 @@
+class Solution {
+   public:
+    int l;
+    vector<int> vis;
+    vector<vector<int>> ans;
+    vector<int> perm;
+    void backtrack(vector<int>& nums, int idx) {
+        if (idx == l) {
+            ans.emplace_back(perm);
+            return;
+        }
+        for (int i = 0; i < l; i++) {
+            if (vis[i] || (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1])) {
+                continue;
+            }
+            perm.emplace_back(nums[i]);
+            vis[i] = 1;
+            backtrack(nums, idx + 1);
+            vis[i] = 0;
+            perm.pop_back();
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        l = nums.size();
+
+        vis.resize(nums.size());
+        sort(nums.begin(), nums.end());
+        backtrack(nums, 0);
+        return ans;
+    }
+};
