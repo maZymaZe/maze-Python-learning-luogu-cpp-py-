85ef4f681e4f774be8fee765339814333d01d65b
maze <782618517@qq.com>
2021-11-27 00:11:35 +0800

    update

--- LeetCode/l315.cpp
+++ LeetCode/l315.cpp
@@ -1,44 +1,56 @@
-int m;
-int lowbit(int x){
-    return (-x)&x;
-}
-void update(vector<int>& ts,int pos){
-    while(pos<m){
-        ts[pos]++;
-        pos+=lowbit(pos);
-    }
-}
-int ffind(vector<int>& ts,int pos){
-    int ans=0;
-    while(pos){
-        ans+=ts[pos];
-        pos-=lowbit(pos);
-    }
-    return ans;
-}
 class Solution {
-public:
+   private:
+    vector<int> c;
+    vector<int> a;
+
+    void Init(int length) { c.resize(length, 0); }
+
+    int LowBit(int x) { return x & (-x); }
+
+    void Update(int pos) {
+        while (pos < c.size()) {
+            c[pos] += 1;
+            pos += LowBit(pos);
+        }
+    }
+
+    int Query(int pos) {
+        int ret = 0;
+
+        while (pos > 0) {
+            ret += c[pos];
+            pos -= LowBit(pos);
+        }
+
+        return ret;
+    }
+
+    void Discretization(vector<int>& nums) {
+        a.assign(nums.begin(), nums.end());
+        sort(a.begin(), a.end());
+        a.erase(unique(a.begin(), a.end()), a.end());
+    }
+
+    int getId(int x) {
+        return lower_bound(a.begin(), a.end(), x) - a.begin() + 1;
+    }
+
+   public:
     vector<int> countSmaller(vector<int>& nums) {
-        int l=nums.size();
-        unordered_set<int> s;
-        vector<int> vec,ans(l);
-        for(auto i:nums){
-            if(!s.count(i)){
-                vec.push_back(i);
-                s.insert(i);
-            }
+        vector<int> resultList;
+
+        Discretization(nums);
+
+        Init(nums.size() + 5);
+
+        for (int i = (int)nums.size() - 1; i >= 0; --i) {
+            int id = getId(nums[i]);
+            resultList.push_back(Query(id - 1));
+            Update(id);
         }
-        sort(vec.begin(),vec.end());
-        for(int i=0;i<l;i++){
-            nums[i]=(upper_bound(vec.begin(),vec.end(),nums[i]-1)-vec.begin())+2;
-            m=max(m,nums[i]);
-        }
-        m*=2;
-        vector<int> ts(m+3);
-        for(int i=l-1;i>=0;i--){
-            ans[i]=ffind(ts,nums[i]-1);
-            update(ts,nums[i]);
-        }
-        return ans;
+
+        reverse(resultList.begin(), resultList.end());
+
+        return resultList;
     }
 };
