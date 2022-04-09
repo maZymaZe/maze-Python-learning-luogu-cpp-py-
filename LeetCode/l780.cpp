345b5093885a3e343dd2ab0a0c4bc694112fa522
maze <782618517@qq.com>
2022-04-09 10:23:28 +0800

    数学

---
+++ LeetCode/l780.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    bool reachingPoints(int sx, int sy, int tx, int ty) {
+        while (tx > sx && ty > sy && tx != ty) {
+            if (tx > ty) {
+                tx %= ty;
+            } else {
+                ty %= tx;
+            }
+        }
+        if (tx == sx && ty == sy) {
+            return true;
+        } else if (tx == sx) {
+            return ty > sy && (ty - sy) % tx == 0;
+        } else if (ty == sy) {
+            return tx > sx && (tx - sx) % ty == 0;
+        } else {
+            return false;
+        }
+    }
+};
