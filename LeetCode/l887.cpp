527b2bf277e07536eebac43af37d5a36ee574e41
maze <782618517@qq.com>
2022-04-18 00:08:57 +0800

    划水

---
+++ LeetCode/l887.cpp
@@ -0,0 +1,23 @@
+class Solution {
+   public:
+    int superEggDrop(int k, int n) {
+        if (n == 1) {
+            return 1;
+        }
+        vector<vector<int>> f(n + 1, vector<int>(k + 1));
+        for (int i = 1; i <= k; ++i) {
+            f[1][i] = 1;
+        }
+        int ans = -1;
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= k; ++j) {
+                f[i][j] = 1 + f[i - 1][j - 1] + f[i - 1][j];
+            }
+            if (f[i][k] >= n) {
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+};
