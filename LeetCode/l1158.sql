ebb7a20b2a685813ce5c8d04c89e28a607ebd395
maze <782618517@qq.com>
2022-04-22 00:02:15 +0800

    划水

---
+++ LeetCode/l1158.sql
@@ -0,0 +1,9 @@
+select Users.user_id as buyer_id, join_date, ifnull(UserBuy.cnt, 0) as orders_in_2019
+from Users
+left join (
+    select buyer_id, count(order_id) cnt 
+    from Orders
+    where order_date between '2019-01-01' and '2019-12-31'
+    group by buyer_id
+) UserBuy
+on Users.user_id = UserBuy.buyer_id
