15b28a02b9a7504ef1b91253bc2d0fa4ecf6be3e
maze <782618517@qq.com>
2022-08-10 00:14:15 +0800

    划水

---
+++ LeetCode/m08.09.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   public:
+    vector<string> ans;
+    int lim;
+    string str;
+    void dfs(int pos, int s) {
+        if (pos == lim) {
+            if (s == 0)
+                ans.push_back(str);
+            return;
+        }
+        if (s) {
+            str.push_back(')');
+            dfs(pos + 1, s - 1);
+            str.pop_back();
+        }
+        str.push_back('(');
+        dfs(pos + 1, s + 1);
+        str.pop_back();
+    }
+    vector<string> generateParenthesis(int n) {
+        lim = 2 * n;
+        dfs(0, 0);
+        return ans;
+    }
+};
