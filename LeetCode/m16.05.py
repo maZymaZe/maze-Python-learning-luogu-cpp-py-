cc371607f19dbf6b1ad4f9ef0433bc9b981feb3d
maze <782618517@qq.com>
2022-08-27 00:03:59 +0800

    划水

---
+++ LeetCode/m16.05.py
@@ -0,0 +1,8 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        t = 5
+        ans = 0
+        while n//t != 0:
+            ans += n//t
+            t *= 5
+        return ans
