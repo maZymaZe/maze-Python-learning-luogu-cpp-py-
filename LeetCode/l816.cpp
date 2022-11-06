d4de3cc6af8ad2b40f904874a02e1bddd0f1f68a
maze <782618517@qq.com>
2022-11-07 00:06:13 +0800

    划水

---
+++ LeetCode/l816.cpp
@@ -0,0 +1,33 @@
+class Solution {
+   public:
+    vector<string> getPos(string s) {
+        vector<string> pos;
+        if (s[0] != '0' || s == "0")
+            pos.push_back(s);
+        for (int p = 1; p < s.size(); ++p) {
+            if ((p != 1 && s[0] == '0') || s.back() == '0')
+                continue;
+            pos.push_back(s.substr(0, p) + "." + s.substr(p));
+        }
+        return pos;
+    }
+    vector<string> ambiguousCoordinates(string s) {
+        int n = s.size() - 2;
+        vector<string> res;
+        s = s.substr(1, s.size() - 2);
+        for (int l = 1; l < n; ++l) {
+            vector<string> lt = getPos(s.substr(0, l));
+            if (lt.empty())
+                continue;
+            vector<string> rt = getPos(s.substr(l));
+            if (rt.empty())
+                continue;
+            for (auto& i : lt) {
+                for (auto& j : rt) {
+                    res.push_back("(" + i + ", " + j + ")");
+                }
+            }
+        }
+        return res;
+    }
+};
