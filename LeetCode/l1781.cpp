7fa8bcd0c40260f7d188bbf633098fcffe636434
maze <782618517@qq.com>
2022-12-12 11:52:06 +0800

    划水

---
+++ LeetCode/l1781.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    int beautySum(string s) {
+        int res = 0;
+        for (int i = 0; i < s.size(); i++) {
+            vector<int> cnt(26);
+            int maxFreq = 0;
+            for (int j = i; j < s.size(); j++) {
+                cnt[s[j] - 'a']++;
+                maxFreq = max(maxFreq, cnt[s[j] - 'a']);
+                int minFreq = s.size();
+                for (int k = 0; k < 26; k++) {
+                    if (cnt[k] > 0) {
+                        minFreq = min(minFreq, cnt[k]);
+                    }
+                }
+                res += maxFreq - minFreq;
+            }
+        }
+        return res;
+    }
+};
