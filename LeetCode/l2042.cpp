2ec3d8bb8e2c711c8e46285f11d9f2ae34e20057
maze <782618517@qq.com>
2023-01-03 08:49:28 +0800

    划水

---
+++ LeetCode/l2042.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    bool areNumbersAscending(string s) {
+        int pre = 0, pos = 0;
+        while (pos < s.size()) {
+            if (isdigit(s[pos])) {
+                int cur = 0;
+                while (pos < s.size() && isdigit(s[pos])) {
+                    cur = cur * 10 + s[pos] - '0';
+                    pos++;
+                }
+                if (cur <= pre) {
+                    return false;
+                }
+                pre = cur;
+            } else {
+                pos++;
+            }
+        }
+        return true;
+    }
+};
