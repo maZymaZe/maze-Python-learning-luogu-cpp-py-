d4f2eed57c1e0f669d589cdcc41c2d2edeace8db
maze <782618517@qq.com>
2023-03-01 00:08:49 +0800

    划水

---
+++ LeetCode/l2373.cpp
@@ -0,0 +1,17 @@
+class Solution {
+   public:
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<int>> res(n - 2, vector<int>(n - 2, 0));
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                for (int x = i; x < i + 3; x++) {
+                    for (int y = j; y < j + 3; y++) {
+                        res[i][j] = max(res[i][j], grid[x][y]);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
