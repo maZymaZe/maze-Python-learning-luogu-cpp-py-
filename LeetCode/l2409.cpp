acb8ebe083a0506fe0e8bc392a964a282214117f
maze <782618517@qq.com>
2023-04-17 00:02:29 +0800

    划水

---
+++ LeetCode/l2409.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countDaysTogether(string arriveAlice, string leaveAlice, string arriveBob, string leaveBob) {
+        vector<int> datesOfMonths = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        vector<int> prefixSum(1, 0);
+        for (int date : datesOfMonths) {
+            prefixSum.emplace_back(prefixSum.back() + date);
+        }
+
+        int arriveAliceDay = calculateDayOfYear(arriveAlice, prefixSum);
+        int leaveAliceDay = calculateDayOfYear(leaveAlice, prefixSum);
+        int arriveBobDay = calculateDayOfYear(arriveBob, prefixSum);
+        int leaveBobDay = calculateDayOfYear(leaveBob, prefixSum);
+        return max(0, min(leaveAliceDay, leaveBobDay) - max(arriveAliceDay, arriveBobDay) + 1);
+    }
+
+    int calculateDayOfYear(string day, const vector<int> &prefixSum) {
+        int month = stoi(day.substr(0, 2));
+        int date = stoi(day.substr(3));
+        return prefixSum[month - 1] + date;
+    }
+};       
+
