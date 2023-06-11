4d41848e41a09d3aa0d9d0f08f64cbc97866007c
maze <782618517@qq.com>
2023-06-12 00:11:59 +0800

    .

---
+++ LeetCode/l1171.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        int prefix = 0;
+        unordered_map<int, ListNode*> seen;
+        for (ListNode* node = dummy; node; node = node->next) {
+            prefix += node->val;
+            seen[prefix] = node;
+        }
+        prefix = 0;
+        for (ListNode* node = dummy; node; node = node->next) {
+            prefix += node->val;
+            node->next = seen[prefix]->next;
+        }
+        return dummy->next;
+    }
+};
