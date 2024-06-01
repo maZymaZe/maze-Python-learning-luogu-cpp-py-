f9b5c9a4ab2b145233e1b96f74ead4cff50b8bdd
maze <782618517@qq.com>
2024-06-01 09:26:29 +0800

    划水

---
+++ LeetCode/l2928.cpp
@@ -0,0 +1,14 @@
+class Solution {
+   public:
+    int distributeCandies(int n, int limit) {
+        int ans = 0;
+        for (int i = 0; i <= limit; i++) {
+            for (int j = 0; j <= limit; j++) {
+                if (i + j <= n && n - i - j <= limit) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
