ad0d1a1e7b2ca385baa311f200b537a179421fc4
maze <782618517@qq.com>
2024-09-30 09:12:18 +0800

    划水

---
+++ LeetCode/l1845.cpp
@@ -0,0 +1,23 @@
+class SeatManager {
+   public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    SeatManager(int n) {
+        for (int i = 1; i <= n; i++)
+            pq.push(i);
+    }
+
+    int reserve() {
+        int ret = pq.top();
+        pq.pop();
+        return ret;
+    }
+
+    void unreserve(int seatNumber) { pq.push(seatNumber); }
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
