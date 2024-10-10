6d029fd0ac94286ddab6630462a7d8d8229f52f4
maze <782618517@qq.com>
2024-10-10 09:36:44 +0800

    划水

---
+++ LeetCode/l3162.cpp
@@ -0,0 +1,13 @@
+class Solution {
+   public:
+    int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size(), m = nums2.size(), ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (nums1[i] % (nums2[j] * k) == 0)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
