83db355a29dac6bcc257b08cc1837aabd61ef5d7
maze <782618517@qq.com>
2025-07-23 08:21:44 +0800

    ..

---
+++ LeetCode/l1717.cpp
@@ -0,0 +1,30 @@
+class Solution {
+   public:
+    int maximumGain(string s, int x, int y) {
+        char a = 'a', b = 'b';
+        if (x < y) {
+            swap(x, y);
+            swap(a, b);
+        }
+
+        int ans = 0, cnt1 = 0, cnt2 = 0;
+        for (char c : s) {
+            if (c == a) {
+                cnt1++;
+            } else if (c == b) {
+                if (cnt1) {
+                    ans += x;
+                    cnt1--;
+                } else {
+                    cnt2++;
+                }
+            } else {
+                ans += min(cnt1, cnt2) * y;
+                cnt1 = 0;
+                cnt2 = 0;
+            }
+        }
+        ans += min(cnt1, cnt2) * y;
+        return ans;
+    }
+};
